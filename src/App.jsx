7288f147cfc6f9c02df3a36afbe103137393eef7
Andrés Felipe Morera Díaz <afmorerad@corre.udistrital.edu.co>
2022-10-03 00:43:58 -0500

    Creating floating WhatsAppButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useRef, useEffect } from "react";
-
 import Music from "./components/Music.jsx";
 import About from "./components/About";
 import Links from "./components/Links";
 import Header from "./components/Header";
 import Logo from "./components/Logo";
-
 import AOS from "aos";
 import "aos/dist/aos.css";
 import animateMainImage from "./scripts/animation.js";
 import { useMediaQuery } from "react-responsive";
+import FloatingWhatsApp from 'react-floating-whatsapp'
+import dummyAvatar from './assets/fotoWhats.jpg';
+
+
+
+
 
 const App = () => {
   const isDesktop = useMediaQuery({
@@ -30,6 +34,7 @@ const App = () => {
     <div className="app-container">
       <Header MusicRef={MusicRef} AboutRef={AboutRef} LinksRef={LinksRef} />
       <Logo />
+      <FloatingWhatsApp phoneNumber="+573133300834" accountName="Andrés Morera" chatMessage="¿Que hay pa' hacer?" avatar={dummyAvatar} darkMode={true} />
       <div className="page" ref={MusicRef}>
         <Music />
       </div>
